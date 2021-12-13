f9ba35958293abfa00cad194da970bb7347e1402
karikov Aleksandr <kaeikov.sanya2803@outlook.com>
2021-12-13 18:40:30 +0700

    Helpers

---
+++ Helpers/ApiHelperUri.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkControllerAdmin.Http.Helper.ApiHelper
+{
+    public static class ApiHelperUri
+    {
+        public static string RegisterUri = "User/register";
+        public static string LoginUri = "User/login";
+    }
+}
